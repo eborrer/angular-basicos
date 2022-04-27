a45316e3f598b6c3a2a6315d3da6797953dcabde
hectorgomex <hgome15@bancodebogota.com.co>
2022-04-27 17:03:10 -0500

    mi primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//import { ContadorComponent } from './contador/contador/contador.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador/contador.module';
 
-@NgModule({
+
+
+
+@NgModule({       
   declarations: [
-    AppComponent
+    AppComponent,
+    //ContadorComponent,
+    //HeroeComponent,
+    //ListadoComponent
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
